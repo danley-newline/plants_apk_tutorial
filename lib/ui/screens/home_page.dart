b613baa015c015ca9292ce7ab25b804ad8562093
daniel.kacou <daniel.kacou@sunu-group.com>
2024-12-02 00:46:34 +0000

    Home Page search bar and Category title ok

--- lib/ui/screens/home_page.dart
+++ lib/ui/screens/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:my_plant_app/constants.dart';
+import 'package:my_plant_app/models/plants.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,9 +12,108 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Home Page"),
+    int selectedIndex = 0;
+    Size size = MediaQuery.of(context).size;
+
+    List<Plant> _plantList = Plant.plantList;
+
+    //Plants Category
+    List<String> _plantTypes = [
+      "Recommended",
+      "Indoor",
+      "Outdoor",
+      "Garden",
+      "Supplement"
+    ];
+
+    //Toggle Favorite button
+    bool toggleIsFavorited(bool isFavorited) {
+      return !isFavorited;
+    }
+
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: const EdgeInsets.only(top: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                    ),
+                    width: size.width * .9,
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(45, 6, 81, 9),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.search,
+                          color: const Color.fromARGB(139, 0, 0, 0),
+                        ),
+                        const Expanded(
+                          child: TextField(
+                            showCursor: false,
+                            decoration: InputDecoration(
+                              hintText: "Search Plant",
+                              border: InputBorder.none,
+                              focusedBorder: InputBorder.none,
+                            ),
+                          ),
+                        ),
+                        Icon(
+                          Icons.mic,
+                          color: const Color.fromARGB(139, 0, 0, 0),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              height: 50.0,
+              width: size.width,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: _plantTypes.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          selectedIndex = index;
+                        });
+                      },
+                      child: Text(
+                        _plantTypes[index],
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: selectedIndex == index
+                              ? FontWeight.bold
+                              : FontWeight.w300,
+                          color: selectedIndex == index
+                              ? Constants.primaryColor
+                              : Constants.blackColor,
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          
+          ],
+        ),
       ),
     );
   }
